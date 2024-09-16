e3284e4f024118e77ccc5e59cf2225203caa1915
HobbitDur <ludovic791@gmail.com>
2024-09-16 11:23:43 +0200

    Correction abilities bug

--- PythonAutomatisation/fshandler.py
+++ PythonAutomatisation/fshandler.py
@@ -14,18 +14,8 @@ def unpack(source_path, dest_path):
     subprocess.run(["Resources/ArchiveManagementCLI/deling-cli.exe", "export", os.path.join(source_path, "battle.fs"), dest_path])
 
 
-def pack(special_battle_path, dest_path):
+def pack(source_path, dest_path):
     """The deling-cli.exe create the .fl by taking the relative path of where the file is executed,
     which is not good for the "official" .fl file."""
-    for file in DELING_LIST_FILES:
-        if os.path.isfile(file):
-            shutil.copy(file, DELING_OUTPUT_ROOT_PATH)
-    wd = os.getcwd()
-    os.chdir(DELING_OUTPUT_ROOT_PATH)
-    subprocess.run(["deling-cli.exe", "import", "-c", "lzs", special_battle_path, "battle.fs", "-f"])
-    os.chdir(wd)
-    for file in DELING_LIST_FILES:
-        file_name = os.path.basename(file)
-        path = os.path.join(dest_path, file_name)
-        if os.path.isfile(path):
-            pathlib.Path.unlink(pathlib.Path(path))
+    os.makedirs(dest_path, exist_ok=True)
+    subprocess.run(["Resources/ArchiveManagementCLI/deling-cli.exe", "import", "-c", "lzs", source_path, os.path.join(dest_path,"battle.fs"), "-f", "--prefix", 'c:\\ff8\\data\\fre\\battle\\'])
