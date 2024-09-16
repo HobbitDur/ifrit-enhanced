38c0bac5ec3d11f8b33101cabc965c2637b557f3
HobbitDur <ludovic791@gmail.com>
2024-09-16 11:23:54 +0200

    Starting on ifrit-AI

---
+++ ifrit-AI/main.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+import sys
+from PyQt6.QtWidgets import QApplication
+
+from ifritai import IfritAI
+
+sys._excepthook = sys.excepthook
+def exception_hook(exctype, value, traceback):
+    print(exctype, value, traceback)
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+if __name__ == '__main__':
+    sys.excepthook = exception_hook
+
+    app = QApplication.instance()
+    if not app:  # sinon on crée une instance de QApplication
+        app = QApplication(sys.argv)
+
+    main_window = IfritAI()
+    sys.exit(app.exec())
