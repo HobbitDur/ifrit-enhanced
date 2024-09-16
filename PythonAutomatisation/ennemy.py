4abeb8ba99f68297ccfd66e6772ae4438e881dff
HobbitDur <ludovic791@gmail.com>
2024-09-16 11:23:44 +0200

    Adding combat text

--- PythonAutomatisation/ennemy.py
+++ PythonAutomatisation/ennemy.py
@@ -13,11 +13,56 @@ class Ennemy():
         self.file_raw_data = bytearray()
         self.data = []
         self.pointer_data_start = 0
+        self.pointer_battle_script_data_start = 0
         self.origin_file_name = ""
         self.origin_file_checksum = ""
+        self.text_battle = []
+        self.text_subsection_offset = 0
 
     def __repr__(self):
         return "Name: {} \nData:{}".format(self.name, self.data)
+
+    def __analyze_battle_section(self, game_data):
+        # Get sub
+        text_subsection_offset_raw_data = self.file_raw_data[self.pointer_battle_script_data_start +
+                                                             game_data.DATA_POINTER_SUBSECTION_OFFSET['offset']:
+                                                             self.pointer_battle_script_data_start +
+                                                             game_data.DATA_POINTER_SUBSECTION_OFFSET['offset'] +
+                                                             game_data.DATA_POINTER_SUBSECTION_OFFSET['size']]
+        self.text_subsection_offset = int.from_bytes(text_subsection_offset_raw_data, byteorder=game_data.DATA_POINTER_SUBSECTION_OFFSET['byteorder'])
+        text_offset_raw_data = self.file_raw_data[self.pointer_battle_script_data_start +
+                                                  game_data.DATA_POINTER_TEXT_OFFSET['offset']:
+                                                  self.pointer_battle_script_data_start +
+                                                  game_data.DATA_POINTER_TEXT_OFFSET['offset'] +
+                                                  game_data.DATA_POINTER_TEXT_OFFSET['size']]
+
+        text_offset = int.from_bytes(text_offset_raw_data, byteorder=game_data.DATA_POINTER_TEXT_OFFSET['byteorder'])
+        # We estimate that the nb of offset value is the diff between the two section
+        nb_text = self.text_subsection_offset - text_offset
+
+        text_list_pointer = []
+        for i in range(0, nb_text, 2):
+            text_list_raw_data = self.file_raw_data[self.pointer_battle_script_data_start + text_offset + i:
+                                                    self.pointer_battle_script_data_start + text_offset +
+                                                    game_data.BATTLE_TEXT_OFFSET['size'] + i]
+            if i > 0 and text_list_raw_data == b'\x00\x00':  # Weird case where there is several pointer by the diff but several are 0 (which point to the same value)
+                break
+            text_list_pointer.append(int.from_bytes(text_list_raw_data, byteorder=game_data.BATTLE_TEXT_OFFSET['byteorder']))
+
+        for index, text_pointer in enumerate(text_list_pointer):
+            combat_text_raw_data = bytearray()
+            for i in range(100):
+                if self.pointer_battle_script_data_start + self.text_subsection_offset + text_pointer + i >= len(self.file_raw_data):
+                    # Shouldn't happen, only on garbage data
+                    pass
+                else:
+                    raw_value = self.file_raw_data[self.pointer_battle_script_data_start + self.text_subsection_offset + text_pointer + i]
+                    if raw_value != 0:
+                        combat_text_raw_data.extend(int.to_bytes(raw_value))
+                    else:
+                        break
+            text = game_data.translate_hex_to_str(combat_text_raw_data)
+            self.text_battle.append(text)
 
     def load_file_data(self, file, game_data):
         with open(file, "rb") as f:
@@ -28,9 +73,12 @@ class Ennemy():
         if '123' in file:  # Weird case, the 123 file is a garbage anyway but this is to avoid error
             data_pointer = 4
         else:
-            data_pointer = game_data.USEFUL_DATA_POINTER['offset']
-        self.pointer_data_start = int.from_bytes(self.file_raw_data[data_pointer:data_pointer + game_data.USEFUL_DATA_POINTER['size']],
-                                                 game_data.USEFUL_DATA_POINTER["byteorder"])
+            data_pointer = game_data.DATA_POINTER_INFO_STAT['offset']
+        self.pointer_data_start = int.from_bytes(self.file_raw_data[data_pointer:data_pointer + game_data.DATA_POINTER_INFO_STAT['size']],
+                                                 game_data.DATA_POINTER_INFO_STAT["byteorder"])
+        data_pointer = game_data.DATA_POINTER_BATTLE_SCRIPT['offset']
+        self.pointer_battle_script_data_start = int.from_bytes(self.file_raw_data[data_pointer:data_pointer + game_data.DATA_POINTER_BATTLE_SCRIPT['size']],
+                                                               game_data.DATA_POINTER_BATTLE_SCRIPT["byteorder"])
         self.origin_file_name = os.path.basename(file)
         self.origin_file_checksum = get_checksum(file, algorithm='SHA256')
 
@@ -39,21 +87,8 @@ class Ennemy():
         name_data = self.file_raw_data[
                     self.pointer_data_start + game_data.NAME_DATA['offset']:  self.pointer_data_start + game_data.NAME_DATA['offset'] + game_data.NAME_DATA[
                         'size']]
-        for el in name_data:
-            if el < 0x20:
-                break
-            else:
-                self.name += Ennemy.translate_hex_to_str(el - 0x20)
 
-    def translate_hex_to_str(hex):
-        with open("Resources/sysfnt.txt", "r", encoding="utf-8") as localize_file:
-            file = localize_file.read()
-            file = file.replace('\n', '')
-            file = file.split("\",\"")
-            file[0] = file[0][1:]  # Special case for the split
-            file[-1] = file[-1][:-1]  # Special case for the split
-
-        return file[hex]
+        self.name += game_data.translate_hex_to_str(name_data)
 
     def analyse_loaded_data(self, game_data):
         """This is the main function. Here we have several cases.
@@ -99,16 +134,20 @@ class Ennemy():
 
             self.data.append({'name': el['name'], 'value': value, 'pretty_name': el['pretty_name']})
 
+        self.__analyze_battle_section(game_data)
+
     def write_data_to_file(self, game_data, path):
         # First copy original file
         full_dest_path = os.path.join(path, self.origin_file_name)
 
         # Then load file (python make it difficult to directly modify files)
         self.load_file_data(full_dest_path, game_data)
+        self.analyse_loaded_data(game_data)
 
         # Then modify loaded file
         for el in self.data:
-            property_elem = [x for ind, x in enumerate(game_data.LIST_DATA) if x['name'] == el['name']][0]
+            if el['name'] not in ['combat_text']:  # Combat txt handled differently
+                property_elem = [x for ind, x in enumerate(game_data.LIST_DATA) if x['name'] == el['name']][0]
             if el['name'] in (game_data.stat_values + ['card', 'devour']):  # List
                 value_to_set = bytes(el['value'])
             elif el['name'] in ['med_lvl', 'high_lvl', 'extra_xp', 'xp', 'ap']:
@@ -139,6 +178,14 @@ class Ennemy():
                     value_to_set.extend(el2['animation'].to_bytes())
                     value_to_set.extend(el2['id'].to_bytes(2, property_elem['byteorder']))
                 value_to_set = bytes(value_to_set)
+            elif el['name'] in ['combat_text']:  # Special case for text as the value change for each text
+                value_to_set = None
+                value_battle = bytearray()
+                for str_battle in el['value']:
+                    value_battle.extend(game_data.translate_str_to_hex(str_battle))
+                    value_battle.extend([0])
+                self.file_raw_data[self.pointer_battle_script_data_start + self.text_subsection_offset:
+                                   self.pointer_battle_script_data_start + self.text_subsection_offset + len(value_battle)] = value_battle
             if value_to_set:
                 self.file_raw_data[self.pointer_data_start + property_elem['offset']:
                                    self.pointer_data_start + property_elem['offset'] + property_elem['size']] = value_to_set
--- PythonAutomatisation/gamedata.py
+++ PythonAutomatisation/gamedata.py
@@ -2,7 +2,7 @@ import os
 
 
 class GameData():
-    USEFUL_DATA_POINTER = {'offset': 0x1C, 'size': 4, 'byteorder': 'little'}
+    DATA_POINTER_INFO_STAT = {'offset': 0x1C, 'size': 4, 'byteorder': 'little'}
     COM127_POINTER_OFFSET = 4
     NAME_DATA = {'offset': 0x00, 'size': 24, 'byteorder': 'big', 'name': 'name', 'pretty_name': 'Name'}
     HP_DATA = {'offset': 0x18, 'size': 4, 'byteorder': 'big', 'name': 'hp', 'pretty_name': 'HP'}
@@ -37,6 +37,12 @@ class GameData():
     ABILITIES_LOW_DATA = {'offset': 0x34, 'size': 64, 'byteorder': 'little', 'name': 'abilities_low', 'pretty_name': 'Abilities Low Level'}
     ABILITIES_MED_DATA = {'offset': 0x74, 'size': 64, 'byteorder': 'little', 'name': 'abilities_med', 'pretty_name': 'Abilities Medium Level'}
     ABILITIES_HIGH_DATA = {'offset': 0xB4, 'size': 64, 'byteorder': 'little', 'name': 'abilities_high', 'pretty_name': 'Abilities High Level'}
+
+    DATA_POINTER_BATTLE_SCRIPT = {'offset': 0x20, 'size': 4, 'byteorder': 'little'}
+    DATA_POINTER_SUBSECTION_OFFSET = {'offset': 0x0C, 'size': 4, 'byteorder': 'little'}
+    DATA_POINTER_TEXT_OFFSET = {'offset': 0x08, 'size': 4, 'byteorder': 'little'}
+    BATTLE_TEXT_OFFSET = {'offset': 0x00, 'size': 2, 'byteorder': 'little'}
+
     LIST_DATA = [HP_DATA, STR_DATA, VIT_DATA, MAG_DATA, SPR_DATA, SPD_DATA, EVA_DATA, MED_LVL_DATA, HIGH_LVL_DATA, EXTRA_XP_DATA, XP_DATA, LOW_LVL_MAG_DATA,
                  MED_LVL_MAG_DATA, HIGH_LVL_MAG_DATA, LOW_LVL_MUG_DATA, MED_LVL_MUG_DATA,
                  HIGH_LVL_MUG_DATA, LOW_LVL_DROP_DATA, MED_LVL_DROP_DATA, HIGH_LVL_DROP_DATA, MUG_RATE_DATA, DROP_RATE_DATA, AP_DATA, ELEM_DEF_DATA,
@@ -45,6 +51,11 @@ class GameData():
     MISC_ORDER = ['med_lvl', 'high_lvl', 'extra_xp', 'xp', 'mug_rate', 'drop_rate', 'ap']
     ABILITIES_HIGHNESS_ORDER = ['abilities_low', 'abilities_med', 'abilities_high']
     RESOURCE_FOLDER = "Resources"
+    CHARACTER_LIST = ["Squall", "Zell", "Irvine", "Quistis", "Rinoa", "Selphie", "Seifer", "Edea", "Laguna", "Kiros", "Ward", "Angelo",
+                      "Griever", "Boko"]
+    COLOR_LIST = ["Darkgrey", "Grey", "Yellow", "Red", "Green", "Blue", "Purple", "White",
+                  "DarkgreyBlink", "GreyBlink", "YellowBlink", "RedBlink", "GreenBlink", "BlueBlink", "PurpleBlink", "WhiteBlink"]
+    LOCATION_LIST = ["Galbadia", "Esthar", "Balamb", "Dollet", "Timber", "Trabia", "Centra", "Horizon"]
 
     def __init__(self):
         self.devour_values = []
@@ -56,6 +67,192 @@ class GameData():
         self.stat_values = []
         self.ennemy_abilities_values = {}
         self.ennemy_abilities_type_values = {}
+        self.translate_hex_to_str_table = []
+        self.__init_hex_to_str_table()
+
+    def __init_hex_to_str_table(self):
+        with open("Resources/sysfnt.txt", "r", encoding="utf-8") as localize_file:
+            self.translate_hex_to_str_table = localize_file.read()
+            self.translate_hex_to_str_table = self.translate_hex_to_str_table.replace(',",",',
+                                                                                      ',";;;",')  # Handling the unique case of a "," character (which is also a separator)
+            self.translate_hex_to_str_table = self.translate_hex_to_str_table.replace('\n', '')
+            self.translate_hex_to_str_table = self.translate_hex_to_str_table.split(',')
+            for i in range(len(self.translate_hex_to_str_table)):
+                self.translate_hex_to_str_table[i] = self.translate_hex_to_str_table[i].replace(';;;', ',')
+                if self.translate_hex_to_str_table[i].count('"') == 2:
+                    self.translate_hex_to_str_table[i] = self.translate_hex_to_str_table[i].replace('"', '')
+
+    def translate_str_to_hex(self, string):
+        c = 0
+        str_size = len(string)
+        encode_list = []
+        while c < str_size:
+            char = string[c]
+            if char == '\n':  # \n{NewPage}\n,\n
+                if '{NewPage}' in string[c + 1:c + 10]:
+                    encode_list.append(0x01)
+                    c += 10
+                else:
+                    encode_list.append(0x02)
+                    c += 1
+                continue
+            elif char == '{':
+                rest = string[c+1:]
+                index_next_bracket = rest.find('}')
+                if index_next_bracket != -1:
+                    substring = rest[:index_next_bracket]
+                    if substring in self.CHARACTER_LIST:  # {name}
+                        index_list = self.CHARACTER_LIST.index(substring)
+                        if index_list < 11:
+                            encode_list.extend([0x03, 0x30 + index_list])
+                        elif index_list == 11:
+                            encode_list.extend([0x03, 0x40])
+                        elif index_list == 12:
+                            encode_list.extend([0x03, 0x50])
+                        elif index_list == 13:
+                            encode_list.extend([0x03, 0x60])
+                    elif substring in self.COLOR_LIST:  # {Color}
+                        index_list = self.COLOR_LIST.index(substring)
+                        encode_list.extend([0x06, 0x20 + index_list])
+                    elif substring in self.LOCATION_LIST:  # {Location}
+                        index_list = self.LOCATION_LIST.index(substring)
+                        encode_list.extend([0x0e, 0x20 + index_list])
+                    elif 'Var' in substring:
+                        if len(substring) == 5:
+                            if 'b' in substring:  # {Varb0}
+                                encode_list.extend([0x04, int(substring[-1]) + 0x40])
+                            else:  # {Var00}
+                                encode_list.extend([0x04, int(substring[-1]) + 0x30])
+                        else:  # {Var0}
+                            encode_list.extend([0x04, int(substring[-1]) + 0x20])
+                    elif 'Wait' in substring:  # {Wait000}
+                        encode_list.extend([0x09, int(substring[-1]) + 0x20])
+                    elif 'Jp' in substring:  # {Jp000}
+                        encode_list.extend([0x1c, int(substring[-1]) + 0x20])
+                    elif '{' + substring + '}' in self.translate_hex_to_str_table:  # {} at end of sysfnt
+                        encode_list.append(self.translate_hex_to_str_table[self.translate_hex_to_str_table.index('{' + substring + '}')])
+                    elif 'x' in substring and len(substring) == 5:  # {xffff}
+                        encode_list.extend([int(substring[1:3], 16), int(substring[3:5], 16)])
+                    elif 'x' in substring and len(substring) == 3:  # {xff}
+                        encode_list.append(int(substring[1:3], 16))
+                    c += len(substring) + 2
+                    continue
+            encode_list.append(self.translate_hex_to_str_table.index(char))
+            c += 1
+            # Jp ?
+        return encode_list
+
+
+
+    def translate_hex_to_str(self, hex_list):
+        str = ""
+        i = 0
+        hex_size = len(hex_list)
+        while i < hex_size:
+            hex_val = hex_list[i]
+
+            if hex_val == 0x00:
+                pass
+            elif hex_val in [0x01, 0x02]:
+                str += self.translate_hex_to_str_table[i]
+            elif hex_val == 0x03:  # {Name}
+                i += 1
+                if i < hex_size:
+                    hex_val = hex_list[i]
+                    if hex_val >= 0x30 and hex_val <= 0x3a:
+                        str += '{' + self.CHARACTER_LIST[hex_val - 0x30] + '}'
+                    elif hex_val == 0x40:
+                        str += '{' + self.CHARACTER_LIST[11] + '}'
+                    elif hex_val == 0x50:
+                        str += '{' + self.CHARACTER_LIST[12] + '}'
+                    elif hex_val == 0x60:
+                        str += '{' + self.CHARACTER_LIST[13] + '}'
+                    else:
+                        str += "{{x03{:02x}}}".format(hex_val)
+                else:
+                    str += "{x03}"
+            elif hex_val == 0x04:  # {Var0}, {Var00} et {Varb0}
+                i += 1
+                if hex_val < hex_size:
+                    hex_val = hex_list[i]
+                    if hex_val >= 0x20 and i <= 0x27:
+                        str += "{{Var{:02x}}}".format(hex_val - 0x20)
+                    elif hex_val >= 0x30 and i <= 0x37:
+                        str += "{{Var0{:02x}}}".format(hex_val - 0x30)
+                    elif hex_val >= 0x40 and i <= 0x47:
+                        str += "{{Varb{:02x}}}".format(hex_val - 0x40)
+                    else:
+                        str += "{{x04{:02x}}}".format(hex_val)
+
+                else:
+                    str += "{x04}"
+            elif hex_val == 0x06:  # {Color}
+                i += 1
+                if i < hex_size:
+                    hex_val = hex_list[i]
+                    if hex_val >= 0x20 and hex_val <= 0x2f:
+                        str += '{' + self.COLOR_LIST[hex_val - 0x20] + '}'
+                    else:
+                        str += "{{x06{:02x}}".format(hex_val)
+                else:
+                    str += "{x06}"
+            elif hex_val == 0x09:  # {Wait000}
+                i += 1
+                if i < hex_size:
+                    hex_val = hex_list[i]
+                    if hex_val >= 0x20:
+                        str += "{{Wait{:03}}}".format(hex_val - 0x20)
+                    else:
+                        str += "{{x09{:02x}}}".format(hex_val)
+                else:
+                    str += "{x06}"
+            elif hex_val == 0x0e:  # {Location}
+                i += 1
+                if i < hex_size:
+                    hex_val = hex_list[i]
+                    if hex_val >= 0x20 and hex_val <= 0x27:
+                        str += '{' + self.LOCATION_LIST[hex_val - 0x20] + '}'
+                    else:
+                        str += "{{x0e{:02x}}}".format(hex_val)
+                else:
+                    str += "{x0e}"
+            elif hex_val >= 0x019 and hex_val <= 0x1b:  # jp19, jp1a, jp1b
+                i += 1
+                if i < len(hex_list):
+                    old_hex_val = hex_val
+                    hex_val = hex_list[i]
+                    if hex_val >= 0x20:
+                        character = None  # To be changed, caract(index, oldIndex-0x18);
+                    else:
+                        character = None
+                    if not character:
+                        character = "{{x{:02x}{:02x}}}".format(old_hex_val, hex_val)
+                    str += character
+                else:
+                    str += "{{x{:02x}}}".format(hex_val)
+            elif hex_val == 0x1c:  # addJp
+                i += 1
+                if i < hex_size:
+                    hex_val = hex_list[i]
+                    if hex_val >= 0x20:
+                        str += "{{Jp{:03}}}".format(hex_val - 0x20)
+                    else:
+                        str += "{{x1c{:02x}}}".format(hex_val)
+                else:
+                    str += "{x1c}"
+            elif hex_val >= 0x05 and hex_val <= 0x1f:
+                i += 1
+                if i < hex_size:
+                    str += "{{x{:02x}{:02x}}}".format(hex_val, hex_list[i])
+                else:
+                    str += "{{x{:02x}}}".format(hex_val)
+            else:
+                character = self.translate_hex_to_str_table[hex_val]  # To be done
+                if not character:
+                    character = "{{x{:02x}}}".format(hex_val)
+                str += character
+            i += 1
+        return str
 
     def load_all(self):
         self.load_card_data(os.path.join(self.RESOURCE_FOLDER, "card.txt"))
@@ -85,14 +282,14 @@ class GameData():
             file_split = f.read().split('\n')
             for el_split in file_split:
                 self.magic_values[int(el_split.split('<')[0], 16)] = {'name': el_split.split('<')[1],
-                                                                             'ref': str(int(el_split.split('<')[0], 16)) + ":"+ el_split.split('<')[1]}
+                                                                      'ref': str(int(el_split.split('<')[0], 16)) + ":" + el_split.split('<')[1]}
 
     def load_item_data(self, file):
         with (open(file, "r") as f):
             file_split = f.read().split('\n')
             for el_split in file_split:
                 self.item_values[int(el_split.split('<')[0], 16)] = {'name': el_split.split('<')[1],
-                                                                             'ref': str(int(el_split.split('<')[0], 16)) + ":"+ el_split.split('<')[1]}
+                                                                     'ref': str(int(el_split.split('<')[0], 16)) + ":" + el_split.split('<')[1]}
 
     def load_status_data(self, file):
         with (open(file, "r") as f):
@@ -101,6 +298,7 @@ class GameData():
     def load_magic_type_data(self, file):
         with (open(file, "r") as f):
             self.magic_type_values = f.read().split('\n')
+
     def load_stat_data(self, file):
         with (open(file, "r") as f):
             self.stat_values = f.read().split('\n')
@@ -110,7 +308,7 @@ class GameData():
             file_split = f.read().split('\n')
             for el_split in file_split:
                 self.ennemy_abilities_values[int(el_split.split('>')[0])] = {'name': el_split.split('>')[1],
-                                                                             'ref': el_split.split('>')[0] + ":"+ el_split.split('>')[1]}
+                                                                             'ref': el_split.split('>')[0] + ":" + el_split.split('>')[1]}
 
     def load_ennemy_abilities_type_data(self, file):
         with (open(file, "r") as f):
