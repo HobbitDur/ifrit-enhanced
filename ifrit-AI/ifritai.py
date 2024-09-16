7258cf78186561da5c99076e61e7b188903fc382
HobbitDur <ludovic791@gmail.com>
2024-09-16 11:23:54 +0200

    Need to change QGridLayout to QV

--- ifrit-AI/ifritai.py
+++ ifrit-AI/ifritai.py
@@ -11,6 +11,8 @@ from ifritmanager import IfritManager
 class IfritAI(QWidget):
     ADD_LINE_SELECTOR_ITEMS = ["Condition", "Command"]
     MAX_COMMAND_PARAM = 7
+    MAX_OP_ID = 61
+    MAX_OP_CODE_VALUE = 255
 
     def __init__(self, icon_path='Resources'):
 
@@ -31,6 +33,10 @@ class IfritAI(QWidget):
         self.setMinimumSize(1280, 720)
         # self.setWindowIcon(QIcon(os.path.join(icon_path, 'icon.png')))
 
+        self.save_button = QPushButton()
+        self.save_button.setIcon(QIcon(os.path.join(icon_path, 'save.svg')))
+        self.save_button.setFixedSize(30, 30)
+        self.save_button.clicked.connect(self.__save_file)
         self.layout_main = QVBoxLayout()
 
         self.file_dialog = QFileDialog()
@@ -38,6 +44,11 @@ class IfritAI(QWidget):
         self.file_dialog_button.setIcon(QIcon(os.path.join(icon_path, 'folder.png')))
         self.file_dialog_button.setFixedSize(30, 30)
         self.file_dialog_button.clicked.connect(self.__load_file)
+
+        self.layout_top = QHBoxLayout()
+        self.layout_top.addWidget(self.file_dialog_button)
+        self.layout_top.addWidget(self.save_button)
+        self.layout_top.addStretch(1)
 
         self.ai_lines_layout = QGridLayout()
 
@@ -52,7 +63,7 @@ class IfritAI(QWidget):
         # self.add_line_layout.addWidget(self.add_line_selector)
 
         self.scroll_widget.setLayout(self.layout_main)
-        self.layout_main.addWidget(self.file_dialog_button)
+        self.layout_main.addLayout(self.layout_top)
         self.layout_main.addLayout(self.ai_lines_layout)
         # self.layout_main.addLayout(self.add_line_layout)
 
@@ -61,54 +72,85 @@ class IfritAI(QWidget):
         self.__load_file()
         self.show()
 
+    def __save_file(self):
+        print("Save file")
+
+
     def __init_add_line(self):
         for i in range(len(self.add_line_button)):
             self.add_line_button[i].setText("+")
             self.add_line_button[i].clicked.connect(lambda: self.__add_line(i))
             self.add_line_selector[i].addItems(self.ADD_LINE_SELECTOR_ITEMS)
 
-    def __add_line(self, line_index, command):
+    def __delete_line(self, command):
+        print("Delete line !")
+        for col in range(self.ai_lines_layout.count()):
+            item = self.ai_lines_layout.
+            print(item)
+            if item:
+                self.ai_lines_layout.removeWidget(item)
+            else:
+                break
+
+    def __add_line(self, command):
         if command.get_id() == 35:
             self.if_column_index -= 1
-        op_id_widget = QSpinBox()
-        op_id_widget.setValue(command.get_id())
-        op_id_widget.setMaximumWidth(40)
+        command.op_id_widget = QSpinBox()
+        command.op_id_widget.setMaximumWidth(40)
+        command.op_id_widget.setValue(command.get_id())
+        command.op_id_widget.setMaximum(self.MAX_OP_ID)
+        command.op_id_widget.valueChanged.connect(lambda: self.__op_id_change(command))
         frame = QFrame()
         frame.setFrameStyle(0x05)
         frame.setLineWidth(2)
         layout_id = QHBoxLayout()
-        layout_id.addWidget(op_id_widget)
+        layout_id.addWidget(command.op_id_widget)
         layout_id.addWidget(frame)
-        self.ai_lines_layout.addLayout(layout_id, line_index, 0)
+        self.ai_lines_layout.addLayout(layout_id, command.line_index, 0)
 
-        #self.ai_lines_layout.addWidget(op_id_widget, line_index, 0)
-        column_index =1
-        print(len(command.get_op_code()))
-        for code in command.get_op_code():
-            op_code_widget = QSpinBox()
-            op_code_widget.setValue(code)
-            op_code_widget.setMaximumWidth(40)
-            op_code_widget.setMaximum(255)
-            op_code_widget.setMinimum(0)
-            op_code_widget.valueChanged.connect(lambda: self.__op_code_change(column_index-1))
-            self.ai_lines_layout.addWidget(op_code_widget, line_index, column_index)
-            column_index += 1
+        self.__reset_op_code_widget(command)
 
         frame_text = QFrame()
         frame_text.setFrameStyle(0x05)
         frame_text.setLineWidth(2)
         column_index = self.MAX_COMMAND_PARAM + 1
-        text_widget = QLabel(command.get_text())
+        command.text_widget = QLabel(command.get_text())
         layout_text = QHBoxLayout()
         layout_text.addWidget(frame_text)
         layout_text.addSpacing(20*self.if_column_index)
-        layout_text.addWidget(text_widget)
-        self.ai_lines_layout.addLayout(layout_text, line_index, column_index)
+        layout_text.addWidget(command.text_widget)
+        self.ai_lines_layout.addLayout(layout_text, command.line_index, column_index)
         if command.get_id() == 2: #IF
             self.if_column_index+=1
 
-    def __op_code_change(self, index_op_code):
-        print(index_op_code)
+    def __reset_op_code_widget(self, command):
+        column_index =1
+        print(len(command.get_op_code()))
+        command.op_code_widget = []
+        for i, code in enumerate(command.get_op_code()):
+            command.op_code_widget.append(QSpinBox())
+            command.op_code_widget[i].setMaximumWidth(40)
+            command.op_code_widget[i].setMaximum(self.MAX_OP_CODE_VALUE)
+            command.op_code_widget[i].setMinimum(0)
+            command.op_code_widget[i].setValue(code)
+            command.op_code_widget[i].valueChanged.connect(lambda: self.__op_code_change(command))
+            self.ai_lines_layout.addWidget(command.op_code_widget[i], command.line_index, column_index)
+            column_index += 1
+
+    def __op_id_change(self, command):
+        command.set_op_id(command.op_id_widget.value())
+        self.__op_code_change(command)
+        self.__delete_line(command)
+        self.__add_line(command)
+        command.text_widget.setText(command.get_text())
+
+    def __op_code_change(self, command):
+        op_code = []
+        for widget in command.op_code_widget:
+            op_code.append(widget.value())
+        command.set_op_code(op_code)
+        command.text_widget.setText(command.get_text())
+        print(command.get_op_code())
 
     def __load_file(self):
         # file_name = self.file_dialog.getOpenFileName(parent=self, caption="Search dat file", filter="*.dat", directory=os.getcwd())[0]
@@ -124,8 +166,8 @@ class IfritAI(QWidget):
         self.__clear_layout(self.ai_lines_layout)
         for code_section in self.ifrit_manager.ai_data:
             for command in code_section:
-                print(command)
-                self.__add_line(line_index, command)
+                command.line_index = line_index
+                self.__add_line(command)
                 line_index+=1
         print(self.ai_lines_layout.count())
 
